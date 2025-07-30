0cdcde70e6324712601e198b6ea83a46d2a7372c
kpthanh01 <kphamthanh01@gmail.com>
2025-07-30 10:55:12 -0400

    adding project image selection when over hovering over text; added placeholder text for icon on navbar

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,17 @@
 import { Link } from "react-router"
 import styles from "./Home.module.scss"
+import { useState } from "react"
 
 const Home = () => {
+  const [image, setImage] = useState("project1");
+
+  const onHover = (e) => {
+    e.preventDefault();
+    setRevealImg(e.target.id);
+  }
+
   return (
     <div className="home-container">
-      home
       <section className={`${styles.header} container-fluid`}>
         <img className={styles.headshot} src="https://e7.pngegg.com/pngimages/188/424/png-clipart-software-developer-computer-servers-programmer-web-developer-computer-software-others-miscellaneous-face.png" alt="headshot" />
         <h1 className="title">Kevin Thanh Pham. Full Stack Developer based in Georgia.</h1>
@@ -22,12 +29,33 @@ const Home = () => {
         <h2>Selected Projects</h2>
         <p>Below is a few of my most recent projects. To view all of my works, head over to the <Link>projects page</Link>.</p>
         <div className={styles.projectContainer}>
-          <img id="porjectSnapshot" src="https://naxotop.com/wp-content/uploads/2022/12/New-Project-10.webp" alt="project snapshot" />
+          <div>
+            <img
+              src="https://naxotop.com/wp-content/uploads/2022/12/New-Project-10.webp"
+              alt="project snapshot"
+              className={image === "project1" ? styles.imgShow : styles.imgHidden}
+            />
+             <img
+              src="https://naxotop.com/wp-content/uploads/2022/12/New-Project-9.webp"
+              alt="project snapshot"
+              className={image === "project2" ? styles.imgShow : styles.imgHidden}
+            />
+             <img
+              src="https://naxotop.com/wp-content/uploads/2022/12/New-Project-8.webp"
+              alt="project snapshot"
+              className={image === "project3" ? styles.imgShow : styles.imgHidden}
+            />
+             <img
+              src="https://naxotop.com/wp-content/uploads/2022/12/New-Project-7.webp"
+              alt="project snapshot"
+              className={image === "project4" ? styles.imgShow : styles.imgHidden}
+            />
+          </div>
           <div className="project-list">
-            <div>Atlanta-Kravings</div>
-            <div>Jane's Voice Actor Site</div>
-            <div>Bicycle Racerz</div>
-            <div>Octopath Traveler Encyclopedia</div>
+            <div onMouseOver={() => setImage("project1")}>Atlanta-Kravings</div>
+            <div onMouseOver={() => setImage("project2")}>Jane's Voice Actor Site</div>
+            <div onMouseOver={() => setImage("project3")}>Bicycle Racerz</div>
+            <div onMouseOver={() => setImage("project4")}>Octopath Traveler Encyclopedia</div>
           </div>
         </div>
       </section>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,7 +6,11 @@ const Navbar = () => {
     <div>
       <nav className="navbar navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark">
         <div className="container-fluid">
-          <span className={styles.title}>Kevin Pham</span>
+          <div>
+            <span className={styles.title}>Kevin Pham</span>
+            <a className="ms-3" target="_blank" rel="noopener nonreferrer" href="https://github.com/kpthanh01">test</a>
+            <a className="ms-3" target="_blank" rel="noopener nonreferrer" href="https://www.linkedin.com/in/phamktp/">boo</a>
+          </div>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
